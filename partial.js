b5a2e0c4e3db8d16a67ca0811863cb289a047cee
TseHang <a54383813@gmail.com>
2016-12-04 11:24:13 +0800

    init

---
+++ partial.js
@@ -0,0 +1,15 @@
+var fs = require("fs");
+
+module.exports= function (hbs) {
+  // register partials
+  hbs.registerPartial('head', getPartials('head'));
+  hbs.registerPartial('header', getPartials('header'));
+  hbs.registerPartial('footer', getPartials('footer'));
+  hbs.registerPartial('nav', getPartials('nav'));
+};
+
+function getPartials(filename) {
+  var template = fs.readFileSync('./layout/partial/'+filename+'.hbs', 'utf8');
+  template = template.replace(/[\t\n]/g, '');
+  return template;
+}
---
+++ route.js
@@ -0,0 +1,71 @@
+/*
+  It's all by  `canner-core` to transform
+*/
+var route = [
+    {
+      data: {
+        path: './',
+        title: 'TFT - 為臺灣而教',
+        first_word: 'It is a good template'
+      },
+      partials: './partial.js',
+      layout:  "./layout/index.hbs",
+      filename: "./index.html"
+    },
+    {
+      data: {
+        path: './',
+        title: '問題與解方',
+        first_word: 'It is a good template'
+      },
+      partials: './partial.js',
+      layout:  "./layout/solution.hbs",
+      filename: "./solution.html"
+    },{
+      data: {
+        path: './',
+        title: '改變與成果',
+        first_word: 'It is a good template'
+      },
+      partials: './partial.js',
+      layout:  "./layout/achievment.hbs",
+      filename: "./achievment.html"
+    },{
+      data: {
+        path: './',
+        title: '申請與甄選',
+        first_word: 'It is a good template'
+      },
+      partials: './partial.js',
+      layout:  "./layout/apply.hbs",
+      filename: "./apply.html"
+    },{
+      data: {
+        path: './',
+        title: '資源與收穫',
+        first_word: 'It is a good template'
+      },
+      partials: './partial.js',
+      layout:  "./layout/gain.hbs",
+      filename: "./gain.html"
+    },{
+      data: {
+        path: './',
+        title: '招募活動',
+        first_word: 'It is a good template'
+      },
+      partials: './partial.js',
+      layout:  "./layout/recruit.hbs",
+      filename: "./recruit.html"
+    },{
+      data: {
+        path: './',
+        title: 'FAQ',
+        first_word: 'It is a good template'
+      },
+      partials: './partial.js',
+      layout:  "./layout/faq.hbs",
+      filename: "./faq.html"
+    }
+  ];
+module.exports = route;
