82d6fd1fad784bf8ad8854c62767f49aaaf1ca05
Rex Tsou <akccakccwww@gmail.com>
2016-12-08 02:21:14 +0800

    edit header(menu)

--- dist/js/index.js
+++ dist/js/index.js
@@ -48,3 +48,12 @@ var initializeClock = function initializeClock(id, endtime) {
 
 var deadline = new Date(2016, 11, 31); //2016/12/31--24:00
 initializeClock('countdownDIV', deadline);
+
+document.addEventListener('DOMContentLoaded', function () {
+  var menuBtn = document.querySelector('.menu_button');
+  var menuList = document.querySelector('.m-nav--mobile');
+  menuBtn.addEventListener('click', function (e) {
+    menuBtn.classList.toggle('is-active');
+    menuList.classList.toggle('is-active');
+  });
+});
--- js/index.js
+++ js/index.js
@@ -51,3 +51,12 @@ const initializeClock = (id, endtime) => {
 
 const deadline = new Date(2016, 11, 31); //2016/12/31--24:00
 initializeClock('countdownDIV', deadline);
+
+document.addEventListener('DOMContentLoaded', ()=> {
+  const menuBtn = document.querySelector('.menu_button');
+  const menuList = document.querySelector('.m-nav--mobile');
+  menuBtn.addEventListener('click', (e)=> {
+    menuBtn.classList.toggle('is-active');
+    menuList.classList.toggle('is-active');
+  });
+});
