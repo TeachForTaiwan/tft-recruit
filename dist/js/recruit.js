d90d2f9f4f2a0995bef38d9af1e770df8c98a737
TseHang <a54383813@gmail.com>
2016-12-09 09:35:38 +0800

    fix link

--- dist/js/recruit.js
+++ dist/js/recruit.js
@@ -3,13 +3,13 @@
 var deadline = new Date(2016, 11, 31); //2016/12/31--24:00
 
 var curSelectId = 'info';
-$('.btn').click(function () {
+$('.btn-section').click(function () {
 		if (this.id === curSelectId) {
 				;
 		} else {
 				$('#recruit-' + curSelectId).addClass('is-hidden');
 				$('#recruit-' + this.id).removeClass('is-hidden');
-				$('.btn').toggleClass('btn-recruit-disable');
+				$('.btn-section').toggleClass('btn-recruit-disable');
 
 				window.location.hash = '#recruit-' + this.id;
 
