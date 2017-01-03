a2f056a13becda6123dad8a251e41961029f11ce
Rex Tsou <akccakccwww@gmail.com>
2017-01-03 22:59:01 +0800

    add gulp-livereload && edit gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 const gulp = require('gulp');
-const compass = require('gulp-compass') ;
+const compass = require('gulp-compass');
 const uglify = require('gulp-uglify');
 const plumber = require('gulp-plumber');
 const minifyCSS = require('gulp-minify-css');
 const babel = require('gulp-babel');
+const livereload = require('gulp-livereload'); // live reload
 
 const imagemin = require('gulp-imagemin');
 const concat = require('gulp-concat');
@@ -11,46 +12,47 @@ const rename = require('gulp-rename');
 
 const chalk = require('chalk');
 
- 
 /*
 	`gulp default`
 	sasss , js , watch
 */
-gulp.task('sass',function(){
-	return gulp.src(['./sass/**/*.scss'])
+gulp.task('sass', () => {
+	gulp.src(['./sass/**/*.scss'])
 		.pipe(plumber())
 		.pipe(compass({
 			config_file: './config.rb',
-    	css: './dist/css',
-    	sass: 'sass',
-    	comments: false  // Don't include `comments`
+			sass: 'sass',
+			css: './dist/css',
+			comments: false  // Don't include `comments`
 		}))
-		.pipe(minifyCSS())
+		// .pipe(minifyCSS())
 		.pipe(gulp.dest('dist/css'))
-})
+		.pipe(livereload());
+});
 
-gulp.task('js', function(){
-	return gulp.src(['./js/**/*.js'])
-		.pipe(plumber())	
+gulp.task('js', () => {
+	gulp.src(['./js/**/*.js'])
+		.pipe(plumber())
 		.pipe(babel({
-      presets: ['es2015']
-    }))
+			presets: ['es2015']
+		}))
     /* -- TODO --
      MINIFY js ,if readt to online!!
 		*/
 		// .pipe(uglify())
-		.pipe(rename(function(path){
-			logPath(path , "yellow");
-    }))
+		.pipe(rename(path => {
+			logPath(path, "yellow");
+		}))
 		.pipe(gulp.dest('dist/js'))
-})
+});
 
-gulp.task('watch',function(){
+gulp.task('watch', () => {
+	livereload.listen();
 	gulp.watch(['./sass/**/*.scss'], ['sass']);
-  gulp.watch(['./js/**/*.js'], ['js']);
-})
+	gulp.watch(['./js/**/*.js'], ['js']);
+});
 
-gulp.task('default',['sass','js','watch']);
+gulp.task('default', ['sass', 'js', 'watch']);
 
 
 /*
@@ -64,88 +66,88 @@ gulp.task('default',['sass','js','watch']);
     ...
   })
 */
-gulp.task('concate-css', function () {
-  gulp.src(['src/*.css'])
-  	.pipe(plumber())
-   	.pipe(concat('all.css'))
-    .pipe(gulp.dest('lib/'));
+gulp.task('concate-css', () => {
+	gulp.src(['src/*.css'])
+		.pipe(plumber())
+		.pipe(concat('all.css'))
+		.pipe(gulp.dest('lib/'));
 });
 
-gulp.task('concate-js', function () {
-  gulp.src(['src/*.js'])
-  	.pipe(plumber())
-   	.pipe(concat('all.js'))
-    .pipe(gulp.dest('lib/'));
+gulp.task('concate-js', () => {
+	gulp.src(['src/*.js'])
+		.pipe(plumber())
+		.pipe(concat('all.js'))
+		.pipe(gulp.dest('lib/'));
 });
 
-gulp.task('minify-css', function(){
+gulp.task('minify-css', () => {
 	gulp.src(['src/**/*.css'])
 		.pipe(plumber())
 		.pipe(minifyCSS({
 			keepBreaks: true,
 		}))
-		.pipe(rename(function(path){
-    	path.basename += ".min" ;
-    	path.extname = ".css";
+		.pipe(rename(path => {
+			path.basename += ".min";
+			path.extname = ".css";
 
-    	logPath(path , "green");
-    }))
+			logPath(path, "green");
+		}))
 		.pipe(gulp.dest('lib/'))
-})
+});
 
-gulp.task('minify-js', function(){
+gulp.task('minify-js', () => {
 	gulp.src(['src/**/*.js'])
 		.pipe(plumber())
 		.pipe(uglify())
-		.pipe(rename(function(path){
-			path.basename += '.min' ;
-			path.extname = '.js' ;
+		.pipe(rename(path => {
+			path.basename += '.min';
+			path.extname = '.js';
 
-			logPath(path,"yellow");
+			logPath(path, "yellow");
 		}))
 		.pipe(gulp.dest('lib/'))
-})
+});
 
 /*
 	`gulp image` 
 	When your project is completed , you can compress images
 */
-gulp.task('image', function () {
-  gulp.src('dist/img/**/**')
-  	.pipe(plumber())
-   	.pipe(imagemin())
-    .pipe(gulp.dest('dist/img/'));
+gulp.task('image', () => {
+	gulp.src('dist/img/**/**')
+		.pipe(plumber())
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/img/'));
 });
 
 
 
-function logPath(path , color) {
-	switch(color){
+function logPath(path, color) {
+	switch (color) {
 		case "red":
-			color = "31" ;
-			break ;
+			color = "31";
+			break;
 		case "green":
-			color = "32" ;
-			break ;
+			color = "32";
+			break;
 		case "yellow":
-			color = "33" ;
-			break ;
+			color = "33";
+			break;
 		case "blue":
-			color = "34" ;
-			break ;
+			color = "34";
+			break;
 		case "purple":
-			color = "35" ;
-			break ;
+			color = "35";
+			break;
 		case "sky-blue":
-			color = "36" ;
-			break ;
+			color = "36";
+			break;
 		case "white":
-			color = "37" ;
-			break ;
+			color = "37";
+			break;
 	}
 
-  if (path.dirname === ".")
-    console.log("\n \033["+color+"m finish \033[0m '" + path.basename + path.extname + "'");
-  else
-    console.log("\n \033["+color+"m finish \033[0m '" + path.dirname + path.basename + path.extname + "'");
+	if (path.dirname === ".")
+		console.log("\n \033[" + color + "m finish \033[0m '" + path.basename + path.extname + "'");
+	else
+		console.log("\n \033[" + color + "m finish \033[0m '" + path.dirname + path.basename + path.extname + "'");
 }
