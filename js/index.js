3684408b6958558475a0d630071779d8278bca18
Rex Tsou <akccakccwww@gmail.com>
2017-01-07 18:14:49 +0800

    edit teacher story

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const deadline = new Date(2017, 4, 5); //2016/12/31--24:00
 initializeClock('countdownDIV', deadline);
 
+
 // document.addEventListener('DOMContentLoaded', ()=> {
 //   const menuBtn = document.querySelector('.menu_button');
 //   const menuList = document.querySelector('.m-nav--mobile');
