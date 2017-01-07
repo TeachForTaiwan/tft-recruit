cd67818939c779820558ae6a85ea9fd39ceb9a52
TseHang <a54383813@gmail.com>
2017-01-07 12:08:15 +0800

    fix code layout & style

--- dist/js/global.js
+++ dist/js/global.js
@@ -44,3 +44,16 @@ var initializeClock = function initializeClock(id, endtime) {
 };
 
 // -------------------------------
+
+// Nav-mobile Bar
+document.addEventListener('DOMContentLoaded', function () {
+  var menuBtn = document.querySelector('.menu_button');
+  var menuList = document.querySelector('.m-nav--mobile');
+  var mask = document.querySelector('.overlay');
+
+  menuBtn.addEventListener('click', function (e) {
+    menuBtn.classList.toggle('is-active');
+    menuList.classList.toggle('is-active');
+    mask.classList.toggle('is-active');
+  });
+});
--- js/global.js
+++ js/global.js
@@ -46,3 +46,17 @@ const initializeClock = (id, endtime) => {
 }
 
 // -------------------------------
+
+// Nav-mobile Bar
+document.addEventListener('DOMContentLoaded', () => {
+  const menuBtn = document.querySelector('.menu_button');
+  const menuList = document.querySelector('.m-nav--mobile');
+  const mask = document.querySelector('.overlay');
+
+  menuBtn.addEventListener('click', (e) => {
+    menuBtn.classList.toggle('is-active');
+    menuList.classList.toggle('is-active');
+    mask.classList.toggle('is-active');
+  });
+
+});
