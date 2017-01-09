4b597efafe10c083a148cc19ae7ccb5a23348e66
TseHang <a54383813@gmail.com>
2017-01-09 14:02:54 +0800

    fix img

--- dist/js/global.js
+++ dist/js/global.js
@@ -1,1 +1,101 @@
-"use strict";var getTimeRemaining=function(e){var t=Date.parse(e)-Date.parse(new Date),n=Math.floor(t/1e3%60),o=Math.floor(t/1e3/60%60),i=Math.floor(t/36e5%24),r=Math.floor(t/864e5);return{total:t,days:r,hours:i,minutes:o,seconds:n}},initializeClock=function(e,t){var n=document.getElementById(e),o=n.querySelector(".days"),i=n.querySelector(".hours"),r=n.querySelector(".minutes"),a=(n.querySelector(".seconds"),function(){var e=getTimeRemaining(t);o.innerHTML=e.days,i.innerHTML=("0"+e.hours).slice(-2),r.innerHTML=("0"+e.minutes).slice(-2),e.total<=0&&clearInterval(s)});a();var s=setInterval(a,1e3)};document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".menu_button"),t=document.querySelector(".m-nav--mobile"),n=document.querySelector(".overlay"),o=$(".wrap"),i=$(".wrap .quote").innerHeight(),r=document.getElementById("gDoc").dataset.name,a=function(e){window.innerWidth<768?o.css({"margin-bottom":e}):o.css({"margin-bottom":"initial"})},s=function(e){gDoc&&gDoc("1JP0tnjFoTQO388FpDELkfM318KSxFMbJgkCaNgR_WZQ",r),e()},c=function(){setTimeout(function(){document.querySelector(".loading-mask").classList.remove("is-loading")},500)};a(i),e.addEventListener("click",function(o){e.classList.toggle("is-active"),t.classList.toggle("is-active"),n.classList.toggle("is-active")}),window.addEventListener("load",function(){s(c)}),window.addEventListener("resize",function(){var e=$(".wrap .quote").innerHeight();a(e)})});
+'use strict';
+
+/*
+ *  Get TimeRemaining
+ */
+var getTimeRemaining = function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor(t / 1000 % 60);
+  var minutes = Math.floor(t / 1000 / 60 % 60);
+  var hours = Math.floor(t / (1000 * 60 * 60) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+};
+
+var initializeClock = function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+  var schedule = [['Jul 25 2015', 'Sept 20 2015'], ['Sept 21 2015', 'Jul 25 2016'], ['Jul 25 2016', 'Jul 25 2030']];
+
+  var updateClock = function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    // secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  };
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+};
+
+// -------------------------------
+
+// ----- Main CODE -------
+
+
+// ----- DOMContentLoaded -----
+document.addEventListener('DOMContentLoaded', function () {
+
+  var menuBtn = document.querySelector('.menu_button');
+  var menuList = document.querySelector('.m-nav--mobile');
+  var mask = document.querySelector('.overlay');
+  var $banner = $('.wrap');
+  var quoteH = $('.wrap .quote').innerHeight();
+  var gDocTabName = document.getElementById('gDoc').dataset.name;
+  var setBannerBottom = function setBannerBottom(quoteH) {
+    if (window.innerWidth < 768) {
+      $banner.css({
+        "margin-bottom": quoteH
+      });
+    } else {
+      $banner.css({
+        "margin-bottom": "initial"
+      });
+    }
+  };
+  var getGDoc = function getGDoc(callback) {
+    if (gDoc) {
+      gDoc('1JP0tnjFoTQO388FpDELkfM318KSxFMbJgkCaNgR_WZQ', gDocTabName);
+    }
+    callback();
+  };
+  var loadingAnimEnd = function loadingAnimEnd() {
+    setTimeout(function () {
+      document.querySelector('.loading-mask').classList.remove('is-loading');
+    }, 500);
+  };
+
+  setBannerBottom(quoteH);
+
+  menuBtn.addEventListener('click', function (e) {
+    menuBtn.classList.toggle('is-active');
+    menuList.classList.toggle('is-active');
+    mask.classList.toggle('is-active');
+  });
+
+  // ----- onLoad -----
+  window.addEventListener('load', function () {
+    getGDoc(loadingAnimEnd);
+  });
+
+  // ----- onResize -----
+  window.addEventListener('resize', function () {
+    var quoteH = $('.wrap .quote').innerHeight();
+    setBannerBottom(quoteH);
+  });
+});
